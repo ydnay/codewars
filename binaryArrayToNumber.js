ea695752b9f91f9668d8026f922a426e72f5ef60
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-10-28 01:20:20 -0400

    Add binaryArrayToNumber

---
+++ binaryArrayToNumber.js
@@ -0,0 +1,15 @@
+// Given an array of one's and zero's convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+console.log(binaryArrayToNumber([0, 0, 0, 1])) // 1
+console.log(binaryArrayToNumber([0, 0, 1, 0])) // 2
+console.log(binaryArrayToNumber([0, 1, 0, 1])) // 5
+console.log(binaryArrayToNumber([1, 0, 0, 1])) // 9
+console.log(binaryArrayToNumber([0, 0, 1, 0])) // 2
+console.log(binaryArrayToNumber([0, 1, 1, 0])) // 6
+console.log(binaryArrayToNumber([1, 1, 1, 1])) // 15
+console.log(binaryArrayToNumber([1, 0, 1, 1])) // 11
