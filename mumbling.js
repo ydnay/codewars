a7de49914ed8928d6fb63d832cdf5eb8c2c125a0
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-10-31 20:21:40 -0400

    Refactor accum

--- mumbling.js
+++ mumbling.js
@@ -1,8 +1,10 @@
 // This time no story, no theory. The examples below show you how to write function accum:
 
-function accum(s) {
-  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
-}
+// function accum(s) {
+//   return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+// }
+
+const accum = str => str.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 
 console.log(accum("abcd"));    // "A-Bb-Ccc-Dddd"
 console.log(accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
